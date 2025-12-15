86240e762fff51340a1115a986dfdab93a32aa3d
Piotrek Budzowski <piobudzows@gmail.com>
2025-12-15 14:01:29 +0100

    cambios

---
+++ metroMalaga/src/main/java/metroMalaga/View/PanelSMTP.java
@@ -0,0 +1,15 @@
+package metroMalaga.View;
+
+import javax.swing.JFrame;
+
+import metroMalaga.Model.Usuario;
+
+public class PanelSMTP extends JFrame {
+
+	private Usuario user;
+
+	public PanelSMTP(Usuario user) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
