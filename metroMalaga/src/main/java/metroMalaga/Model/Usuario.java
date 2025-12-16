760a267e49ba542e6aa06f999fe6d8d3624848f9
Piotrek Budzowski <piobudzows@gmail.com>
2025-12-16 09:58:18 +0100

    .

--- metroMalaga/src/main/java/metroMalaga/Model/Usuario.java
+++ metroMalaga/src/main/java/metroMalaga/Model/Usuario.java
@@ -1,5 +1,22 @@
 package metroMalaga.Model;
 
 public class Usuario {
+    private String usernameApp;   
+    private String passwordApp;  
+    private String emailReal;
 
+    public Usuario(String usernameApp, String passwordApp, String emailReal) {
+        this.usernameApp = usernameApp;
+        this.passwordApp = passwordApp;
+        this.emailReal = emailReal;
+    }
+
+    public String getUsernameApp() { return usernameApp; }
+    public void setUsernameApp(String usernameApp) { this.usernameApp = usernameApp; }
+
+    public String getPasswordApp() { return passwordApp; }
+    public void setPasswordApp(String passwordApp) { this.passwordApp = passwordApp; }
+
+    public String getEmailReal() { return emailReal; }
+    public void setEmailReal(String emailReal) { this.emailReal = emailReal; }
 }
