e822b7565ffd0ab582edc08894c922db36c84665
Piotrek Budzowski <piobudzows@gmail.com>
2025-12-17 10:41:21 +0100

    .

--- metroMalaga/src/main/java/metroMalaga/Controller/menu/MenuSelect.java
+++ metroMalaga/src/main/java/metroMalaga/Controller/menu/MenuSelect.java
@@ -12,7 +12,12 @@ import org.apache.commons.net.ftp.FTPFile;
 
 import metroMalaga.Controller.CrudController;
 import metroMalaga.Controller.ServiceFTP;
-import metroMalaga.Controller.ftp.FTPRefreshThread;
+import metroMalaga.Controller.ftp.FTPbtnNewFolder;
+import metroMalaga.Controller.ftp.FTPbtnReturn;
+import metroMalaga.Controller.ftp.FTPbtnUp;
+import metroMalaga.Controller.ftp.FTPbtnUpFile;
+import metroMalaga.Controller.ftp.FTPdoubleClick;
+import metroMalaga.Controller.ftp.FTPlist;
 import metroMalaga.Controller.smtp.HandleSMTP;
 import metroMalaga.Model.FTPTableModel;
 import metroMalaga.Model.Usuario;
@@ -46,45 +51,55 @@ public class MenuSelect implements ActionListener {
 		String nameButton = button.getText();
 
 		switch (nameButton) {
-			case "CRUD":
-				CrudFrontend crudView = new CrudFrontend(user);
-				CrudController crudControl = new CrudController(crudView);
-				crudView.setVisible(true);
-				break;
+		case "CRUD":
+			CrudFrontend crudView = new CrudFrontend(user);
+			CrudController crudControl = new CrudController(crudView);
+			crudView.setVisible(true);
+			break;
 
-			case "FTP":
-				ServiceFTP service = new ServiceFTP(user.getRol().getPermiso());
-				FTPFile[] fileArray = service.listAllFiles();
-				List<FTPFile> initialFiles = new ArrayList<>(Arrays.asList(fileArray));
+		case "FTP":
+			startFTP();
+			break;
 
-				FTPTableModel ftpModel = new FTPTableModel(initialFiles, service);
-				// Initialize notification system with the table model
-				service.setTableModel(ftpModel);
-				PanelFTP panelFtp = new PanelFTP(user, service, initialFiles, ftpModel);
-				/*
-				 * FTPRefreshThread refreshThread = new FTPRefreshThread(service, ftpModel);
-				 * refreshThread.start();
-				 * 
-				 * panelFtp.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-				 * 
-				 * panelFtp.addWindowListener(new java.awt.event.WindowAdapter() {
-				 * 
-				 * @Override public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-				 * if (refreshThread != null) { refreshThread.stopRunning(); }
-				 * panelFtp.dispose(); } });
-				 */
-				panelFtp.setVisible(true);
-				break;
+		case "SMTP":
+			HandleSMTP handleSmtp = new HandleSMTP();
+			PanelSMTP panelSmtp = new PanelSMTP(user, handleSmtp);
+			panelSmtp.setVisible(true);
+			break;
 
-			case "SMTP":
-				HandleSMTP handleSmtp = new HandleSMTP();
-				PanelSMTP panelSmtp = new PanelSMTP(user, handleSmtp);
-				panelSmtp.setVisible(true);
-				break;
-
-			case "Salir":
-				break;
+		case "Salir":
+			break;
 		}
 		panelMenu.disposeWindow();
+	}
+
+	private void startFTP() {
+		ServiceFTP service = new ServiceFTP(user.getRol().getPermiso());
+		FTPFile[] fileArray = service.listAllFiles();
+		List<FTPFile> initialFiles = new ArrayList<>(Arrays.asList(fileArray));
+
+		FTPTableModel ftpModel = new FTPTableModel(initialFiles, service);
+
+		PanelFTP panelFtp = new PanelFTP(user, service, initialFiles, ftpModel);
+		service.setTableModel(ftpModel);
+
+		new FTPlist(panelFtp.getSearchField(), ftpModel);
+		new FTPbtnUpFile(panelFtp.getUploadButton(), service, ftpModel, user);
+		new FTPbtnUp(panelFtp.getUpButton(), service, ftpModel);
+		new FTPdoubleClick(panelFtp.getFileTable(), service, ftpModel);
+		new FTPbtnReturn(panelFtp, panelFtp.getReturnButton(), user);
+		new FTPbtnNewFolder(panelFtp.getFolderButton(), service, ftpModel, user);
+		panelFtp.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+		panelFtp.addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent e) {
+				System.out.println("Cerrando recursos FTP...");
+				service.disconnectNotifications();
+				service.close();
+				panelFtp.dispose();
+			}
+		});
+
 	}
 }
