2857ea20b326679a51df51408af80283b0f7734c
Piotrek Budzowski <piobudzows@gmail.com>
2025-12-18 08:39:14 +0100

    .

---
+++ metroMalaga/src/main/java/metroMalaga/Controller/ServiceSMTP.java
@@ -0,0 +1,31 @@
+package metroMalaga.Controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class ServiceSMTP {
+
+	public ConnecionSQL conSQL = new ConnecionSQL();
+
+	public boolean isEmailInWhitelist(String email) {
+		boolean exists = false;
+		final String SQL = "SELECT COUNT(*) FROM lista_blanca WHERE correo_electronico = ?";
+
+		try (Connection con = conSQL.connect(); PreparedStatement ps = con.prepareStatement(SQL)) {
+			ps.setString(1, email);
+			
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					exists = rs.getInt(1) > 0;
+				}
+			}
+		} catch (SQLException e) {
+			String errorMessage = "Error verifying whitelist: " + e.getMessage();
+			JOptionPane.showMessageDialog(null, errorMessage, "Error SQL", JOptionPane.ERROR_MESSAGE);
+		}
+		return exists;
+	}
+}
---
+++ metroMalaga/src/main/java/metroMalaga/Model/Whitelist.java
@@ -0,0 +1,18 @@
+package metroMalaga.Model;
+
+public class Whitelist {
+
+	private String email;
+
+	public Whitelist(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
