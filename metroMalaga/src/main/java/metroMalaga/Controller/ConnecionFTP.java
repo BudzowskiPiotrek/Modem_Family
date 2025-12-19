2fedc6d074763d70b5b4b3d873e900079a9d758c
Piotrek Budzowski <piobudzows@gmail.com>
2025-12-19 08:16:53 +0100

    .

--- metroMalaga/src/main/java/metroMalaga/Controller/ConnecionFTP.java
+++ metroMalaga/src/main/java/metroMalaga/Controller/ConnecionFTP.java
@@ -6,19 +6,13 @@ import org.apache.commons.net.ftp.FTPClient;
 import metroMalaga.Model.Language;
 
 public class ConnecionFTP {
-	private static final String SERVER = "192.168.1.35";
+	private static final String SERVER = "127.0.0.1";
 	private static final int PORT = 21;
-	private String user;
+	private String user = "proyecto";
 	private static final String PASS = "proyecto";
 
-	public ConnecionFTP(String tipo) {
-		if (tipo.equalsIgnoreCase("admin")) {
-			this.user = "proyecto";
-		} else if (tipo.equalsIgnoreCase("usuario")) {
-			this.user = "write";
-		} else if (tipo.equalsIgnoreCase("super")) {
-			this.user = "read";
-		}
+	public ConnecionFTP() {
+
 	}
 
 	public FTPClient getConnection() {
@@ -41,15 +35,12 @@ public class ConnecionFTP {
 				return ftpClient;
 			} else {
 				closeConnection(ftpClient);
-				JOptionPane.showMessageDialog(null,
-						Language.get(189) + user + Language.get(190),
-						Language.get(147), 
+				JOptionPane.showMessageDialog(null, Language.get(189) + user + Language.get(190), Language.get(147),
 						JOptionPane.ERROR_MESSAGE);
 
 			}
 		} catch (IOException e) {
-			String errorMessage = Language.get(191) + SERVER + ":" + PORT + 
-					Language.get(192) + e.getMessage();
+			String errorMessage = Language.get(191) + SERVER + ":" + PORT + Language.get(192) + e.getMessage();
 			JOptionPane.showMessageDialog(null, errorMessage, Language.get(193), JOptionPane.ERROR_MESSAGE);
 		}
 		return null;
