f182cc80c79611f92abca843b5b47ca7238efd87
Piotrek Budzowski <piobudzows@gmail.com>
2025-12-19 08:48:28 +0100

    .

--- metroMalaga/src/main/java/metroMalaga/Main.java
+++ metroMalaga/src/main/java/metroMalaga/Main.java
@@ -2,7 +2,19 @@ package metroMalaga;
 
 import metroMalaga.View.PanelLogin;
 
+/**
+ * Entry point of the Metro Malaga application. This class is responsible for
+ * bootstrapping the application by initializing and displaying the primary
+ * login interface. * @author Modem Family
+ * 
+ * @version 1.0
+ */
 public class Main {
+
+	/**
+	 * Application main method. Launches the graphical user interface by creating an
+	 * instance of the Login Panel. * @param args Command line arguments (not used).
+	 */
 	public static void main(String[] args) {
 		PanelLogin panelLogin = new PanelLogin();
 		panelLogin.setVisible(true);
