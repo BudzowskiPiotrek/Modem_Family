5329fbbca79c294854532ec2eb06164c612ad5cb
aguykeny <agualcaldereyes@gmail.com>
2025-12-19 09:03:23 +0100

    revisa mas

--- metroMalaga/src/main/java/metroMalaga/Controller/login/LoginEnterKeyListener.java
+++ metroMalaga/src/main/java/metroMalaga/Controller/login/LoginEnterKeyListener.java
@@ -6,11 +6,21 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 
+/**
+ * Key listener that triggers the login button action when the Enter key is
+ * pressed.
+ */
 public class LoginEnterKeyListener extends KeyAdapter {
 
     private final JButton loginButton;
     private final LoginAttempt loginAttempt;
 
+    /**
+     * Constructor for LoginEnterKeyListener.
+     * 
+     * @param loginButton  The login button to simulate a click on.
+     * @param loginAttempt The login attempt logic instance.
+     */
     public LoginEnterKeyListener(JButton loginButton, LoginAttempt loginAttempt) {
         this.loginButton = loginButton;
         this.loginAttempt = loginAttempt;
